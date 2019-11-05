2828da0e9169c398a993a734f156b54086836190
Vladimir <vladimir.ladukov@thefintechlab.com>
2019-11-05 16:42:07 +0200

    Add fresh chat with notification.

--- app/src/main/java/com/example/myappfcm/MainActivity.java
+++ app/src/main/java/com/example/myappfcm/MainActivity.java
@@ -3,12 +3,54 @@ package com.example.myappfcm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.freshchat.consumer.sdk.Freshchat;
+import com.freshchat.consumer.sdk.FreshchatConfig;
+import com.freshchat.consumer.sdk.FreshchatUser;
+import com.freshchat.consumer.sdk.exception.MethodNotAllowedException;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnShowConversations, btnShowFAQs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //init
+        FreshchatConfig freshchatConfig = new FreshchatConfig("b675b02c-df26-49c7-a9f7-da68b7d04af4","71fd7fad-2cff-4901-a1ab-7bd143d8091d");
+        Freshchat.getInstance(getApplicationContext()).init(freshchatConfig);
+
+        //Update user information
+        FreshchatUser user = Freshchat.getInstance(getApplicationContext()).getUser();
+        user.setFirstName("John").setLastName("Doe").setEmail("john@john.doe").setPhone("001", "2542542544");
+        try {
+            Freshchat.getInstance(getApplicationContext()).setUser(user);
+        } catch (MethodNotAllowedException e) {
+            e.printStackTrace();
+        }
+
+        btnShowFAQs = (Button) findViewById(R.id.btnShowFAQs);
+        btnShowConversations = (Button) findViewById(R.id.btnShowConversations);
+        btnShowFAQs.setOnClickListener(viewClickListener);
+        btnShowConversations.setOnClickListener(viewClickListener);
     }
+
+    View.OnClickListener viewClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick (View v) {
+            if(v.getId() == R.id.btnShowFAQs) {
+
+                Freshchat.showFAQs(MainActivity.this);
+
+            } else if(v.getId() == R.id.btnShowConversations) {
+
+                Freshchat.showConversations(MainActivity.this);
+
+            }
+        }
+    };
+
 }
--- app/src/main/java/com/example/myappfcm/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/myappfcm/MyFirebaseMessagingService.java
@@ -8,19 +8,37 @@ import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
-import android.os.IBinder;
+import android.util.Log;
 
 import androidx.core.app.NotificationCompat;
 
+import com.freshchat.consumer.sdk.Freshchat;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import static android.content.ContentValues.TAG;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
         public static int NOTIFICATION_ID = 1;
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        generateNotification(remoteMessage.getNotification().getBody(), remoteMessage.getNotification().getTitle());
-        
+        Log.d(TAG, "Refreshed token: " + "onMessageReceived");
+        if (Freshchat.isFreshchatNotification(remoteMessage)) {
+            Freshchat.handleFcmMessage(this, remoteMessage);
+        } else {
+            // Handle notifications with data payload for your app
+        }
+    }
+    @Override
+    public void onNewToken(String token) {
+        Log.d(TAG, "Refreshed token: " + token);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+
+        Freshchat.getInstance(this).setPushRegistrationToken(token);
+        Log.d("DEVICE TOKEN", token);
     }
 
     private void generateNotification(String body, String title) {
